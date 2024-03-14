95a8b0f7c78fa085cde9751720b5263f3135fc0b
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-03-14 10:22:47 +0300

    Initiated a new Feature  using Git Flow - Pricing Card Feature generated using HTML and CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PricingCardComponent } from './pricing-card/pricing-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PricingCardComponent
   ],
   imports: [
     BrowserModule
