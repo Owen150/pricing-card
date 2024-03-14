b45b4f10066fe16684870527a6d038d66513cf9d
Michael Owen Oduor <michaeloduor50@gmail.com>
2024-03-14 12:21:45 +0300

    Price Card Update - Design Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PricingCardComponent } from './pricing-card/pricing-card.component';
+import { PricingTwoComponent } from './pricing-two/pricing-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PricingCardComponent
+    PricingCardComponent,
+    PricingTwoComponent
   ],
   imports: [
     BrowserModule
